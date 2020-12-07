6c0412d666d86e8c0fa830a476a9e7798d652f1a
Tushar3099 <tusharneogi30@gmail.com>
2020-12-08 02:33:09 +0530

    deploying

---
+++ precache-manifest.55ba39a4c96f6901d07e2cfd60a1038f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a4c8c4291ec102cc6647dd6d3051f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "276d8cfc9318f6e8de63",
+    "url": "./static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6475ac34cd97db5ec78f",
+    "url": "./static/css/main.eaaa4458.chunk.css"
+  },
+  {
+    "revision": "276d8cfc9318f6e8de63",
+    "url": "./static/js/2.16ec7d92.chunk.js"
+  },
+  {
+    "revision": "ce84f45c6e623bd7d8c4f820fd2feb3f",
+    "url": "./static/js/2.16ec7d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6475ac34cd97db5ec78f",
+    "url": "./static/js/main.41c55f78.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
--- src/common/constants.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL = "http://localhost:4000";
-const AUTH_URL = `${BASE_URL}/auth`;
-
-export const LOGIN_URL = `${AUTH_URL}/login`;
-export const SIGN_URL = `${AUTH_URL}/signin`;
