245c117a210b0688adfdeed5835c111e20f13f5c
Tushar3099 <tusharneogi30@gmail.com>
2020-12-08 03:51:17 +0530

    Updates

---
+++ precache-manifest.a27936a3948c25ac3de680fa710a95be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333580c15ee0eb9da0fc69a50a19b093",
+    "url": "/CHATAPP-CLIENT/index.html"
+  },
+  {
+    "revision": "37935299d6c9fe65d428",
+    "url": "/CHATAPP-CLIENT/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "382339c8e0be2b3cc084",
+    "url": "/CHATAPP-CLIENT/static/css/main.6c6a06d5.chunk.css"
+  },
+  {
+    "revision": "37935299d6c9fe65d428",
+    "url": "/CHATAPP-CLIENT/static/js/2.baa4a77d.chunk.js"
+  },
+  {
+    "revision": "ce84f45c6e623bd7d8c4f820fd2feb3f",
+    "url": "/CHATAPP-CLIENT/static/js/2.baa4a77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382339c8e0be2b3cc084",
+    "url": "/CHATAPP-CLIENT/static/js/main.8b69b85f.chunk.js"
+  },
+  {
+    "revision": "ae147c796f1e8626eb0c",
+    "url": "/CHATAPP-CLIENT/static/js/runtime-main.8b781529.js"
+  }
+]);
